0b1352a7a8dc79e21346ffce96b964ef7464dc5d
naztech1 <nazarinayoto@gmail.com>
2026-07-22 15:57:59 +0200

    src/services/taskService.js

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -5,6 +5,15 @@ export async function getTasks(){
   const response = await fetch('${BASE_URL}/tasks ');
   if (!response.ok) {
     throw new Error("Failed to fetch tasks");
-    return response.json();
   }
+  return response.json();
+}
+export async function deleteTask(id) {
+  const response = await fetch(`${BASE_URL}/tasks/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) throw new Error("Failed to delete task");
+
+  return response.json();
 }
